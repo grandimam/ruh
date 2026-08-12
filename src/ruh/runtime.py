6cdd08ce7636768da001b888ceec39f164d82a08
Fauzan <13032255+grandimam@users.noreply.github.com>
2026-08-12 23:54:11 +0400

    updated names from core to runtime, and added empty test case files

---
+++ src/ruh/runtime.py
@@ -0,0 +1,31 @@
+from typing import Callable
+from typing import TypeVar
+
+from concurrent.futures import ThreadPoolExecutor
+
+class Executor:
+
+    MAX_WORKERS: int = 10
+
+    def __init__(
+        self,
+        iterable: SplitIterable,
+        func: Callable,
+        max_workers: int = MAX_WORKERS
+    ):
+        self._iterable = iterable
+        self._func = func
+        self._workers = ThreadPoolExecutor(max_workers = max_workers)
+
+
+    def run(self):
+        it = iter(self._iterable)
+        q = [it]
+        while q:
+            item = q.pop()
+            if item.remaining():
+                l, r = item.split()
+                q.append(l)
+                q.append(r)
+            else:
+                self._workers.submit(self._func, next(item))
